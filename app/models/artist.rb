3aa050827579591a73bf4b1b6714c07977004fab
epittab <ep7m@me.com>
2020-05-27 22:24:03 -0400

    finished Class Methods for Artist model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -30,4 +30,51 @@ class Artist < ApplicationRecord
         return most_influenced  
        
     end 
+
+    def self.most_original
+        hash = {}
+        
+        Artist.all.each do |art|
+            hash[art.name] = art.likeds.count 
+        end
+        least_influenced = hash.sort_by{|k, v| v }.first
+        return least_influenced  
+    end
+
+    def self.least_influential
+        hash = {}
+        
+        Artist.all.each do |art|
+            hash[art.name] = art.likers.count 
+        end
+        most_influential = hash.sort_by{|k, v| v }.first
+        return most_influential  
+    end
+
+    def self.average_influences
+  
+        influences_array = Artist.all.map { |art| art.likeds.count }
+        return influences_array.sum(0.0) / influences_array.length  
+
+    end
+
+    def self.total_artists
+        return Artist.all.count
+    end
+
+    def self.most_followed
+        hash = {}
+        
+        Artist.all.each do |art|
+            hash[art.name] = art.user_artists.count 
+        end
+        most_followed = hash.sort_by{|k, v| v }.last
+        return most_followed  
+    end
+
+
+
+    private
+
+
 end
