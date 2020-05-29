9aafcb824d4eda6f22ed1f65d0d84451fa337bfa
mertoz41 <mertozkaynak@windowslive.com>
2020-05-29 13:04:52 -0400

    css complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,25 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require 'rest-client'
 
-grant_url = "https://accounts.spotify.com/api/token"
-
-RestClient.post(grant_url, headers= {}, body={})
-
-url = "https://api.spotify.com/artists"
-
-RestClient.get(url, header)
 
 User.destroy_all
 Artist.destroy_all
 IsLike.destroy_all
-User.destroy_all
-
-user1 = User.create(first_name: 'Bob', last_name: 'Dole', user_name: 'BDole_juice', password: 'abc123')
-user2 = User.create(first_name: 'Mary', last_name: 'Watkins', user_name: 'MJWat', password: 'abc123')
-user3 = User.create(first_name: 'Jeff', last_name: 'Bridges', user_name: 'JeffB12', password: 'abc123')
+UserArtist.destroy_all
+Comment.destroy_all 
 
 
-
-a1 = IsLike.create(liker: jt, liked: mj)
